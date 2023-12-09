d7d96689474220097bab644054b418f1326a7e44
Ali <Zhanabekov.ali@gmail.com>
2023-12-09 11:27:33 +0600

    feat:add api communication

--- quiz-app/quiz.js
+++ quiz-app/quiz.js
@@ -1,1 +1,143 @@
-const questionsAPI = 'https://5d76bf96515d1a0014085cf9.mockapi.io/quiz';
+const questionsAPI = "https://5d76bf96515d1a0014085cf9.mockapi.io/quiz";
+
+let questions;
+let sum = 0;
+
+const request = async () => {
+  try {
+    const response = await fetch(questionsAPI);
+
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    const data = response.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const getData = async () => {
+  try {
+    questions = await request();
+    console.log(questions);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getData();
+
+const quiz = document.querySelector(".quiz");
+const count = document.querySelector("#aside-counter").querySelector("span");
+
+quiz.addEventListener("change", e => {
+  const targetType = e.target.type;
+  const targetName = e.target.name;
+  const targetValue = e.target.value;
+  const allOptions = document.querySelectorAll(`input[name="${targetName}"]`);
+  if (targetType === "radio") {
+    allOptions.forEach(option => {
+      if (!option.checked) {
+        option.disabled = true;
+        option.style.opacity = "0.5";
+      }
+    });
+
+    if (targetName === "q1") {
+      if (questions[0].answer == targetValue) {
+        const wrapper = e.target.parentNode.parentNode;
+        sum += 1;
+        wrapper.classList.add("correct");
+        console.log(count);
+        count.textContent = `${sum}/5`;
+      } else {
+        const wrapper = e.target.parentNode.parentNode;
+        wrapper.classList.add("incorrect");
+        allOptions.forEach(item => {
+          if (item.value == questions[0].answer) {
+            item.parentNode.parentNode.classList.add("correct");
+          }
+        });
+      }
+    }
+
+    if (targetName === "q2") {
+      if (questions[1].answer == targetValue) {
+        const wrapper = e.target.parentNode.parentNode;
+        wrapper.classList.add("correct");
+        sum += 1;
+        count.textContent = `${sum}/5`;
+      } else {
+        const wrapper = e.target.parentNode.parentNode;
+        wrapper.classList.add("incorrect");
+        allOptions.forEach(item => {
+          if (item.value == questions[1].answer) {
+            item.parentNode.parentNode.classList.add("correct");
+          }
+        });
+      }
+    }
+
+    if (targetName === "q3") {
+      if (questions[2].answer == targetValue) {
+        const wrapper = e.target.parentNode.parentNode;
+        wrapper.classList.add("correct");
+        sum += 1;
+        count.textContent = `${sum}/5`;
+      } else {
+        const wrapper = e.target.parentNode.parentNode;
+        wrapper.classList.add("incorrect");
+        allOptions.forEach(item => {
+          if (item.value == questions[2].answer) {
+            item.parentNode.parentNode.classList.add("correct");
+          }
+        });
+      }
+    }
+
+    if (targetName === "q4") {
+      if (questions[3].answer == targetValue) {
+        const wrapper = e.target.parentNode.parentNode;
+        wrapper.classList.add("correct");
+        sum += 1;
+        count.textContent = `${sum}/5`;
+      } else {
+        const wrapper = e.target.parentNode.parentNode;
+        wrapper.classList.add("incorrect");
+        allOptions.forEach(item => {
+          if (item.value == questions[3].answer) {
+            item.parentNode.parentNode.classList.add("correct");
+          }
+        });
+      }
+    }
+
+    if (targetName === "q5") {
+      if (questions[4].answer == targetValue) {
+        const wrapper = e.target.parentNode.parentNode;
+        wrapper.classList.add("correct");
+        sum += 1;
+        count.textContent = `${sum}/5`;
+      } else {
+        const wrapper = e.target.parentNode.parentNode;
+        wrapper.classList.add("incorrect");
+        allOptions.forEach(item => {
+          if (item.value == questions[4].answer) {
+            item.parentNode.parentNode.classList.add("correct");
+          }
+        });
+      }
+    }
+  }
+});
+
+const btnSubmit = document.querySelector("#btn-submit");
+const modal = document.querySelector("#modal-wrapper");
+
+btnSubmit.addEventListener("click", e => {
+  e.preventDefault();
+  modal.querySelector("span").textContent = sum;
+  modal.style.display = "block";
+});
--- watch-page/script.js
+++ watch-page/script.js
@@ -1,5 +1,59 @@
 // Список видео элементов для показа справа
-const playlistURL = 'https://5d76bf96515d1a0014085cf9.mockapi.io/playlist';
+const playlistURL = "https://5d76bf96515d1a0014085cf9.mockapi.io/playlist";
 
 // Список видео для проигрывания
-const videoURL = 'https://5d76bf96515d1a0014085cf9.mockapi.io/video/';
+const videoURL = "https://5d76bf96515d1a0014085cf9.mockapi.io/video/";
+
+const playListWrapper = document.querySelector("#playlist-wrapper");
+
+let playList;
+
+const request = async () => {
+  try {
+    const response = await fetch(playlistURL);
+
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    const data = response.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const getData = async () => {
+  try {
+    playList = await request();
+
+    playList.forEach((item, index) => {
+      const isActive = index === 0 ? " active-card" : "";
+
+      const card = ` <div id="${item.id}" class="playlist-card${isActive}">
+              <img class="thumbnail" src="${item.thumbnail}" />
+              <h3 class="video-card-title">${item.title}</h3>
+            </div>`;
+
+      playListWrapper.insertAdjacentHTML("beforeend", card);
+    });
+
+    // добавляем слушатель на каждую карточку
+    const cards = document.querySelectorAll(".playlist-card");
+    cards.forEach(item => {
+      item.addEventListener("click", () => addActiveCard(item));
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getData();
+
+function addActiveCard(clickedCard) {
+  const cards = document.querySelectorAll(".playlist-card");
+  cards.forEach(item => {
+    item.classList.remove("active-card");
+  });
+  clickedCard.classList.add("active-card");
+}
